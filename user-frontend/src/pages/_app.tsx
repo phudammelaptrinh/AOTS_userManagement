97bbc2b7ad0c9819167b66fb4c7effdf816928a0
phudammelaptrinh <phuho22112003@gmail.com>
2025-09-09 16:29:05 +0700

    xoa thu muc

--- user-frontend/src/pages/_app.tsx
+++
@@ -1,5 +0,0 @@
-import type { AppProps } from "next/app";
-import "../style/globals.css"
-export default function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
-}
